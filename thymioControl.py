6c08c8e0b978a3349afa50707651c98aa2873735
Skaims <mikael.schaer@epfl.ch>
2024-11-20 19:20:15 +0100

    starting function move(path)

--- thymioControl.py
+++ thymioControl.py
@@ -1,3 +1,7 @@
+import numpy as np
+import math
+from kalman import Kalman
+
 class ThymioControl:
     def __init__(self):
         pass
@@ -10,6 +14,36 @@ class ThymioControl:
     def move(path):
         # Move the robot along the path
         # then remove the heading from the path after moving
+        kalman = Kalman()
+        thymioangle = 0 #remove this line to take the real value of thymio angle
+        thymiox = 3; thymioy = 3 #remove this line to take the real pos of the thymio
+
+        #pos of the robot (put the real value thymio.x or i don't know what)
+        pos = (thymiox, thymioy)
+        #calculate the angle that the thymio need
+        alpha = math.atan2((pos[1] - path[0][1]), (pos[0] - path[0][0]))
+        #move the robot until the cell is reach
+        while pos != path:
+            #change the speed depending the angle of the robot
+            if thymioangle == alpha:
+                thymiospeedR = 20 #change this two line to change the value directly on the thymio
+                thymiospeedL = 20
+            else:
+                #calculate the angle that the thymio need to turn
+                thymioangle = alpha - thymioangle
+                while thymioangle != alpha:
+                    if thymioangle > alpha:
+                        thymiospeedR = 0
+                        thymiospeedL = 20
+                    else:
+                        thymiospeedR = 20
+                        thymiospeedL = 0
+                kalman.kalman_update()
+                thymiospeedR = 20
+                thymiospeedL = 20
+            kalman.kalman_update()
+        #remove the cell from the path when it's reached
+        path.remove(path[0])
         pass
 
     def getMotorSpeeds(self):
