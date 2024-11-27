09bad03cd7ce182daff494c7e4792ddf64fd4b78
federock02 <fede.rock.2002@gmail.com>
2024-11-27 20:56:55 +0100

    Fixed controller, PD control, implemented more basic control with stop and turn

--- thymioControl.py
+++ thymioControl.py
@@ -6,12 +6,17 @@ class ThymioControl:
         self.__angle = None
         self.__oldAngle = None
 
+        self.__timeStep = 0
+
+        self.__prevAngleError = 0
+
         self.__step = 1
 
         # thresholds
         self.__kidnappingThresholdPosition = 20
         self.__kidnappingThresholdAngle = 30 # degrees
-        self.__reachedThreshold = 10 # mm
+        self.__reachedThreshold = 20 # mm
+        self.__angleThreshold = 0.1 # rad
 
         # constant linear speed
         self.__linearSpeed = 60
@@ -20,7 +25,9 @@ class ThymioControl:
         # self.__thymioWheelSpeedConversion = 65.5
         self.__thymioWheelSpeedConversion = 0.43 # (mm/s)/pwd
         # constant proportional parameter for transforming angle into rotational speed
-        self.__thymioRotationalSpeedConversion = 0.55
+        self.__proportionalGain = 0.7
+        # constant derivative parameter for transforming angle into rotational speed
+        self.__derivativeGain = 0.5
         # adjustment for the thymio's wheels
         self.__wheelsAdjustment = 1.1
         # cell to mm conversion
@@ -41,7 +48,11 @@ class ThymioControl:
         self.y_est = 0
         self.theta_est = 0
 
+    def set_timestep(self, timestep):
+        self.__timeStep = timestep
+
     def set_pose(self, position, angle):
+        print("Setting pose, position: ", position, " angle: ", angle)
         self.__pos = [position[0] * self.__cellToMm, position[1] * self.__cellToMm]
         self.__angle = angle
 
@@ -51,7 +62,7 @@ class ThymioControl:
     def set_path(self, path):
         self.__path = []
         for position in path:
-            self.__path.append([position[0] * self.__cellToMm, position[1] * self.__cellToMm])
+            self.__path.append([position[1] * self.__cellToMm, position[0] * self.__cellToMm])
         self.__reduce_path()
         self.__step = 1
 
@@ -70,26 +81,26 @@ class ThymioControl:
     def get_path_cells(self):
         new_path = []
         for position in self.__path:
-            new_path.append([position[0] / self.__cellToMm, position[1] / self.__cellToMm])
+            new_path.append([position[1] / self.__cellToMm, position[0] / self.__cellToMm])
         return new_path
     
     def update_pose(self, position, angle):
         self.__oldPos = self.__pos
         self.__oldAngle = self.__angle
-        self.__pos[0] = position[0] * self.__cellToMm
-        self.__pos[1] = position[1] * self.__cellToMm
+        self.__pos = [position[0] * self.__cellToMm, position[1] * self.__cellToMm]
         self.__angle = angle
     
     def amIKidnapped(self):
         # check if the robot is kidnapped
         return math.sqrt(math.pow(self.__oldPos[0] - self.__pos[0]) + math.pow(self.__oldPos[1] - self.__pos[1])) > self.__kidnappingThresholdPosition or abs(self.__oldAngle - self.__angle) > self.__kidnappingThresholdAngle
     
-    def move(self, position, angle):
+    def move_pd(self, position, angle):
+        goal = False
         # move the robot along the path
 
         #position and angle of the thymio
         self.__angle = angle
-        self.__pos = position * self.__cellToMm
+        self.__pos = position
 
         objective = self.__path[self.__step]
 
@@ -97,6 +108,7 @@ class ThymioControl:
         x_diff = objective[0] - self.__pos[0]
         y_diff = objective[1] - self.__pos[1]
         distance = math.sqrt(x_diff**2 + y_diff**2)
+        print("current objective: ", self.__step)
         print("objective: ", objective, " position: ", self.__pos)
         print("x_diff: ", x_diff, " y_diff: ", y_diff, " distance: ", distance)
 
@@ -114,15 +126,69 @@ class ThymioControl:
                 print("Destination reached")
                 return 0, 0, 0, 0, True
             self.__step += 1
+            print("Next objective: ", self.__step)  
 
-            self.move(position, angle)
+            v, w, wl, wr, goal = self.move(position, angle)
         else:
             # move throwards the next cell in the path
-            v, w = self.__linearSpeed, angleDistance * self.__thymioRotationalSpeedConversion
+            v, w = self.__linearSpeed, angleDistance * self.__proportionalGain + self.__derivativeGain * (angleDistance - self.__prevAngleError) / self.__timeStep
+            self.__prevAngleError = angleDistance
             w = max(min(w, self.__maxAngularSpeed), -self.__maxAngularSpeed)
             wl, wr = self.differentialDrive(v, w)
 
             # find wl and wr with the astolfi controller
+        return v, w, wl, wr, goal
+    
+    def move(self, position, angle):
+        # move the robot along the path
+
+        #position and angle of the thymio
+        self.__angle = angle
+        self.__pos = position
+
+        objective = self.__path[self.__step]
+
+        # calculate the distance between the robot and the objective
+        x_diff = objective[0] - self.__pos[0]
+        y_diff = objective[1] - self.__pos[1]
+        distance = math.sqrt(x_diff**2 + y_diff**2)
+        print("current objective: ", self.__step)
+        print("objective: ", objective, " position: ", self.__pos)
+        print("x_diff: ", x_diff, " y_diff: ", y_diff, " distance: ", distance)
+
+        if distance < self.__reachedThreshold:
+            if self.__step == len(self.__path) - 1:
+                print("Destination reached")
+                return 0, 0, 0, 0, True
+            self.__step += 1
+            print("Next objective: ", self.__step)
+            
+            objective = self.__path[self.__step]
+
+            # calculate the new distance between the robot and the objective
+            x_diff = objective[0] - self.__pos[0]
+            y_diff = objective[1] - self.__pos[1]
+            distance = math.sqrt(x_diff**2 + y_diff**2)
+            print("x_diff: ", x_diff, " y_diff: ", y_diff, " distance: ", distance)
+
+        # calculate the angle between the robot and the objective
+        # normalize the angle between -pi and pi
+        print("my angle: ", self.__angle)
+        print("waypoint angle: ", math.atan2(y_diff, x_diff))
+        print("diff angle: ", math.atan2(y_diff, x_diff) - self.__angle)
+        angleDistance = (math.atan2(y_diff, x_diff) - self.__angle + math.pi) % (2 * math.pi) - math.pi
+        print("angleDistance: ", angleDistance)
+
+        if angleDistance > self.__angleThreshold or angleDistance < -self.__angleThreshold:
+            w = angleDistance * self.__proportionalGain + self.__derivativeGain * (angleDistance - self.__prevAngleError) / self.__timeStep
+            v = 0
+        else:
+            w = 0
+            v = self.__linearSpeed
+
+        w = max(min(w, self.__maxAngularSpeed), -self.__maxAngularSpeed)
+        wl, wr = self.differentialDrive(v, w)
+
         return v, w, wl, wr, False
     
     def differentialDrive(self, v, w):
